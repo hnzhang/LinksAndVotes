9a8bdd68b9f8d387395d28affc3d4e63ad2f1a4d
Harry ZHANG <hnzhang@gmail.com>
2018-09-26 09:12:38 -0700

    separate schema to file, add Mutation example

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 const {GraphQLServer} = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!,
+const links = [{
+    id: '001',
+    url: "playernetwork.com",
+    description: 'A boy in in grade 5 in walnut road elementary, and swims at SurreyKnights Club',
+},{
+    id:'002',
+    url: "fleetwood.com",
+    description: 'A girl in Grade 10 in Fleetwood Park Secondary, and coach at SurreyKnight Swimming Club'
+}];
 
-}
-`;
+let idCount = links.length;
 
 const resolvers = {
     Query: {
-        info: () =>`This is the API of the hackernews Clone`
+        info: () =>`This is the API of the hackernews Clone`,
+        feed: () =>links,
     },
+    Mutation: {
+        post: (root, args) => {
+            let newPost = {
+                id: idCount.toString(),
+                url: args.url,
+                description:args.description
+            };
+            links.push(newPost);
+            return newPost;
+        }
+    }
 };
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 });
 server.start(
