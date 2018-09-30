9913c0ee4dfce550bb2a57bea29a4b2e41a97292
Harry ZHANG <hnzhang@gmail.com>
2018-09-30 15:05:00 -0700

    subscription type for new link

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,12 @@
+function newLinkSubscribe(parent, args, context, info) {
+    return context.db.subscription.link({
+        where: {mutation_in : ['CREATED']},
+    }, info);
+}
+const newLink = {
+    subscribe: newLinkSubscribe,
+}
+
+module.exports = {
+    newLink,
+}
