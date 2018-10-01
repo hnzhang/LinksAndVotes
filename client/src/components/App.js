b1a303034cce4827583bf581250d95da67a34ac1
Harry Zhang <hzhang@ea.com>
2018-10-01 16:40:40 -0700

    add route to the page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import {Switch, Route} from 'react-router-dom';
+
+import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 
@@ -7,8 +10,13 @@ class App extends Component {
   render() {
     return (
       <div>
-      <LinkList />
-      <CreateLink />
+        <Header/>
+        <div>
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path='/create' component={CreateLink} />
+          </Switch>
+        </div>
       </div>
     );
   }
